71607353b843be5e4a12eac870f70ca764e3754c
Pedro Rebello <86128693+PedroRebelloM@users.noreply.github.com>
2026-03-13 16:03:25 -0300

    update
    Remoção do algoritmo para ANFC

--- analysis/analiseTemporal.py
+++ analysis/analiseTemporal.py
@@ -8,11 +8,32 @@ import io
 from datetime import datetime
 
 
-def loadData(fileReference, fileComparison, dateTimeRef):
+def loadData(fileReference, fileComparison, dateTimeRef, dateTimeStartComparison):
     reference = xr.open_dataset(fileReference)
     comparison = xr.open_dataset(fileComparison)
 
     referenceSlice = reference.sel(time=dateTimeRef, method="nearest")
+    comparisonSlice = comparison.sel(time=dateTimeStartComparison, method="nearest")
+    print(referenceSlice)
+    print("="*60)
+    print(comparisonSlice)
+
+
+def main():
+    file_ref = 'analysis\cmems_obs-mob_glo_phy-cur_my_0.25deg_PT1H-i_1771818740018.nc'
+    file_cmp = 'analysis\cmems_obs-mob_glo_phy-cur_nrt_0.25deg_PT1H-i_1771818690342.nc'
+    datetime_str = "2026-06-12T00:00:00"
+    loadData(file_ref, file_cmp, datetime_str, datetime_str)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+    
 
 
 
